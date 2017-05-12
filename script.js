c1ed9d712f21e2067c8491634f1a567a499fa07e
priii <get.priyab@gmail.com>
2017-05-11 17:52:02 -0700

    link works

--- script.js
+++ script.js
@@ -10,10 +10,16 @@ $(function(){
       contentType: 'application/json',
       success: function(data, status){
         //$("#displayArea").html();
-        var data1 = data.items[0].productUrl;
+        //var data1 = data.items[0].productUrl;
+        var data_temp = data.items;
+        console.log(data_temp);
       //  $("#displayArea").prepend('<a href='+ data1+'</a>');
-        $("#displayArea").prepend(data1);
-         console.log(data.items[0].productUrl)
+      for(var i=0; i< data_temp.length; i++){
+        var data = data_temp[i];
+        $("#displayArea").prepend('<li class="result">'+'<a href="'+data.productUrl+'">"'+data.name+'</a>'+'<br></li>');
+        // console.log(data.items[0].productUrl);
+
+       }
         },
       error: function(data){
         alert("could not load data from walmart.")
